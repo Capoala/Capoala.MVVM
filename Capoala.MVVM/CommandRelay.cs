921cbb226de6df69018ecd2e5ecb48e21e96e838
Jonathan Nash <jonathan@capoala.me>
2019-03-02 15:37:33 -0500

    Renamed properties and parameters for clarity.
    Updated summaries.

--- Capoala.MVVM/CommandRelay.cs
+++ Capoala.MVVM/CommandRelay.cs
@@ -11,24 +11,24 @@ namespace Capoala.MVVM
         /// <summary>
         /// Creates a new instance of <see cref="CommandRelay"/>.
         /// </summary>
-        /// <param name="action">The action to invoke.</param>
-        /// <param name="predicate">The method that defines whether the command can execute.</param>
-        public CommandRelay(Action action, Func<bool> predicate = null)
+        /// <param name="executionAction">Defines the method to be called when the command is invoked.</param>
+        /// <param name="canExecutePredicate">Defines the method that determines whether the command can execute in its current state.</param>
+        public CommandRelay(Action executionAction, Func<bool> canExecutePredicate = null)
         {
-            RelayAction = action;
-            Predicate = predicate;
+            ExecutionAction = executionAction;
+            CanExecutePredicate = canExecutePredicate;
         }
 
 
         /// <summary>
-        /// The function which determines whether this object is allowed to execute.
+        /// Defines the method that determines whether the command can execute in its current state.
         /// </summary>
-        private readonly Func<bool> Predicate;
+        private readonly Func<bool> CanExecutePredicate;
 
         /// <summary>
-        /// The <see cref="Action"/> to execute.
+        /// Defines the method to be called when the command is invoked.
         /// </summary>
-        private readonly Action RelayAction;
+        private readonly Action ExecutionAction;
 
 
         /// <summary>
@@ -44,13 +44,13 @@ namespace Capoala.MVVM
         /// <returns>
         /// Returns <see langword="true"/> if this command can be executed; otherwise, <see langword="false"/>.
         /// </returns>
-        public bool CanExecute(object parameter) => Predicate?.Invoke() ?? true;
+        public bool CanExecute(object parameter) => CanExecutePredicate?.Invoke() ?? true;
 
         /// <summary>
         /// Defines the method to be called when the command is invoked.
         /// </summary>
         /// <param name="parameter">Data used by the command.</param>
-        public void Execute(object parameter) => RelayAction?.Invoke();
+        public void Execute(object parameter) => ExecutionAction?.Invoke();
 
         /// <summary>
         /// Notifies the client that the ability to execute the command has changed.
@@ -61,29 +61,30 @@ namespace Capoala.MVVM
     /// <summary>
     /// An implementation of <see cref="ICommand"/>.
     /// </summary>
-    public sealed class CommandRelay<TParameter> : ICommand
+    /// <typeparam name="TExecutionParameter">The type of parameter passed to the command execution delegate.</typeparam>
+    public sealed class CommandRelay<TExecutionParameter> : ICommand
     {
         /// <summary>
         /// Creates a new instance of <see cref="CommandRelay{TParameter}"/>.
         /// </summary>
-        /// <param name="action">The action to invoke.</param>
-        /// <param name="predicate">The method that defines whether the command can execute.</param>
-        public CommandRelay(Action<TParameter> action, Func<bool> predicate = null)
+        /// <param name="executionAction">Defines the method to be called when the command is invoked.</param>
+        /// <param name="canExecutePredicate">Defines the method that determines whether the command can execute in its current state.</param>
+        public CommandRelay(Action<TExecutionParameter> executionAction, Func<bool> canExecutePredicate = null)
         {
-            RelayAction = action;
-            Predicate = predicate;
+            ExecutionAction = executionAction;
+            CanExecutePredicate = canExecutePredicate;
         }
 
 
         /// <summary>
-        /// The function which determines whether this object is allowed to execute.
+        /// Defines the method that determines whether the command can execute in its current state.
         /// </summary>
-        private readonly Func<bool> Predicate;
+        private readonly Func<bool> CanExecutePredicate;
 
         /// <summary>
-        /// The <see cref="Action{T}"/> to execute.
+        /// Defines the method to be called when the command is invoked.
         /// </summary>
-        private readonly Action<TParameter> RelayAction;
+        private readonly Action<TExecutionParameter> ExecutionAction;
 
 
         /// <summary>
@@ -97,17 +98,17 @@ namespace Capoala.MVVM
         /// </summary>
         /// <param name="parameter">The type of object the parameter is.</param>
         /// <returns></returns>
-        public bool CanExecute(object parameter) => Predicate?.Invoke() ?? true;
+        public bool CanExecute(object parameter) => CanExecutePredicate?.Invoke() ?? true;
 
         /// <summary>
         /// Defines the method to be called when the command is invoked.
         /// </summary>
         /// <param name="parameter">Data used by the command.</param>
-        public void Execute(object parameter) => RelayAction?.Invoke((TParameter)parameter);
+        public void Execute(object parameter) => ExecutionAction?.Invoke((TExecutionParameter)parameter);
 
         /// <summary>
         /// Notifies the client that the ability to execute the command has changed.
         /// </summary>
-        public void NotifyCanExecuteDidChange() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);        
+        public void NotifyCanExecuteDidChange() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
     }
 }
